5c3d8a8f23e0c79ccda65327111d2b56644a4ffb
MohammedElmustafa <mohamed.elmustafa@icloud.com>
2024-09-08 15:19:14 +0300

    finished

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -3,35 +3,41 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-  if (!user || user === null || !user.id) {
-    throw new Error("Something went wrong");
-  }
-
-  let dbUser = await prisma.user.findUnique({
-    where: {
-      id: user.id,
-    },
-  });
-
-  if (!dbUser) {
-    dbUser = await prisma.user.create({
-      data: {
+    if (!user || !user.id) {
+      return NextResponse.redirect(
+        process.env.NODE_ENV === "production"
+          ? "https://blog-forge.vercel.app/login"
+          : "http://localhost:3000/login"
+      );
+    }
+    let dbUser = await prisma.user.findUnique({
+      where: {
         id: user.id,
-        firstName: user.given_name ?? "",
-        lastName: user.family_name ?? "",
-        email: user.email ?? "",
-        profileImage:
-          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
+    if (!dbUser) {
+      dbUser = await prisma.user.create({
+        data: {
+          id: user.id,
+          firstName: user.given_name ?? "",
+          lastName: user.family_name ?? "",
+          email: user.email ?? "",
+          profileImage:
+            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+        },
+      });
+    }
+    return NextResponse.redirect(
+      process.env.NODE_ENV === "production"
+        ? "https://blog-forge.vercel.app/dasboard"
+        : "http://localhost:3000/dashboard"
+    );
+  } catch (error) {
+    console.error("Error occurred during user creation or retrieval:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
-
-  return NextResponse.redirect(
-    process.env.NODE_ENV === "production"
-      ? "https://blog-forge.vercel.app/dasboard"
-      : "http://localhost:3000/dashboard"
-  );
 }
