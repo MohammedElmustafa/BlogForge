63db2da9a63b9c405f3f82e6a88b18105af6b179
MohammedElmustafa <mohamed.elmustafa@icloud.com>
2024-09-08 16:04:14 +0300

    finished

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Logo from "@/public/logo-Letter.svg";
 import Image from "next/image";
 import { DashboardItems } from "../components/dashboard/DashboardItems";
-import { CircleUser, DollarSign, Globe, Home } from "lucide-react";
+import { CircleUser, DollarSign, Globe, Home, Menu } from "lucide-react";
 import { ThemeToggle } from "../components/dashboard/ThemeToggle";
 import {
   DropdownMenu,
@@ -33,22 +35,27 @@ export const navLinks = [
 ];
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <section className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+    <section className="relative grid min-h-screen w-full grid-cols-1 md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div
+        className={`fixed inset-y-0 left-0 z-40 w-64 transform bg-muted opacity-100 transition-transform duration-300 ease-in-out md:relative md:translate-x-0 ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Image src={Logo} alt="Logo" className="size-8" />
-
-              <h3 className="text-2xl">
+              <Image src={Logo} alt="Logo" className="w-8 h-8" />
+              <h3 className="text-lg lg:text-2xl">
                 Blog<span className="text-primary">Forge</span>
               </h3>
             </Link>
           </div>
 
           <div className="flex-1">
-            <nav className="grid items-start px-2  font-medium lg:px-4">
+            <nav className="grid items-start px-2 font-medium lg:px-4">
               <DashboardItems />
             </nav>
           </div>
@@ -57,7 +64,14 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-          <div className="ml-auto flex items-center gap-x-5">
+          <button
+            className="block md:hidden"
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+          >
+            <Menu className="h-6 w-6" />
+          </button>
+
+          <div className="ml-auto flex items-center gap-x-3 lg:gap-x-5">
             <ThemeToggle />
 
             <DropdownMenu>
@@ -78,10 +92,18 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
             </DropdownMenu>
           </div>
         </header>
+
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           {children}
         </main>
       </div>
+
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 z-30 bg-black opacity-70 md:hidden"
+          onClick={() => setSidebarOpen(false)}
+        ></div>
+      )}
     </section>
   );
 }
