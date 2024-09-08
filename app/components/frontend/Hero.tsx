4a3128c4fe66e315716925a39437de81116bc106
MohammedElmustafa <mohamed.elmustafa@icloud.com>
2024-09-08 16:19:54 +0300

    finished

--- app/components/frontend/Hero.tsx
+++ app/components/frontend/Hero.tsx
@@ -87,19 +87,19 @@ export async function Hero() {
                 <g filter="url(#filter0_f_10_20)">
                   <path
                     d="M128.6 0H0V322.2L106.2 134.75L128.6 0Z"
-                    fill="#1B5E20"
+                    fill="#03FFE0"
                   ></path>
                   <path
                     d="M0 322.2V400H240H320L106.2 134.75L0 322.2Z"
-                    fill="#1B5E20"
+                    fill="#7C87F8"
                   ></path>
                   <path
                     d="M320 400H400V78.75L106.2 134.75L320 400Z"
-                    fill="#0D47A1"
+                    fill="#4C65E4"
                   ></path>
                   <path
                     d="M400 0H128.6L106.2 134.75L400 78.75V0Z"
-                    fill="#16A34A"
+                    fill="#043AFF"
                   ></path>
                 </g>
               </g>
